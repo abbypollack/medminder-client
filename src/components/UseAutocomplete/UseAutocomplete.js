5156e59180dcb8e1be73076cc55ff3731dfbe116
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-22 21:53:08 -0500

    Added drug selection, interaction search, and user drug management functionality to Drug Interaction Checker

--- src/components/UseAutocomplete/UseAutocomplete.js
+++ src/components/UseAutocomplete/UseAutocomplete.js
@@ -12,10 +12,12 @@ function useAutocomplete(baseUrl) {
     }
     setLoading(true);
     try {
-      const response = await axios.get(`${baseUrl}?terms=${encodeURIComponent(input)}`);
+      const response = await axios.get(`${baseUrl}?terms=${encodeURIComponent(input)}&ef=STRENGTHS_AND_FORMS,RXCUIS`);
       setSuggestions(response.data[3].map((item, index) => ({
         name: item[0],
         rxNormId: response.data[1][index],
+        strengthsAndForms: response.data[2].STRENGTHS_AND_FORMS[index],
+        rxNormIds: response.data[2].RXCUIS[index]
       })));
     } catch (error) {
       console.error('Error fetching autocomplete suggestions:', error);
