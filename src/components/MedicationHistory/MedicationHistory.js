12fe3575b23b1f5415e13ea30ebf90414a28bf04
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-26 17:16:50 -0500

    added a message to show when all medications for the day have been taken

--- src/components/MedicationHistory/MedicationHistory.js
+++ src/components/MedicationHistory/MedicationHistory.js
@@ -7,27 +7,40 @@ function MedicationHistory() {
     const { user } = useContext(AuthContext);
     const [medications, setMedications] = useState([]);
     const [loggedMedications, setLoggedMedications] = useState([]);
+    const [allTaken, setAllTaken] = useState(false);
 
     const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
     useEffect(() => {
-        fetchMedicationsForToday();
-    }, []);
+        const fetchMedicationsForToday = async () => {
+            try {
+                const response = await axios.get(`${SERVER_URL}/api/medications/today`, {
+                    headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` }
+                });
+                const medicationsWithParsedTimes = response.data.map(med => ({
+                    ...med,
+                    reminder_times: JSON.parse(med.reminder_times)
+                }));
+                setMedications(medicationsWithParsedTimes);
+            } catch (error) {
+                console.error('Error fetching medications for today:', error);
+            }
+        };
 
-    const fetchMedicationsForToday = async () => {
-        try {
-            const response = await axios.get(`${SERVER_URL}/api/medications/today`, {
-                headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` }
-            });
-            const medicationsWithParsedTimes = response.data.map(med => ({
-                ...med,
-                reminder_times: JSON.parse(med.reminder_times)
-            }));
-            setMedications(medicationsWithParsedTimes);
-        } catch (error) {
-            console.error('Error fetching medications for today:', error);
-        }
-    };
+        const fetchLoggedMedications = async () => {
+            try {
+                const response = await axios.get(`${SERVER_URL}/api/medications/logged`, {
+                    headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` }
+                });
+                setLoggedMedications(response.data);
+            } catch (error) {
+                console.error('Error fetching logged medications:', error);
+            }
+        };
+
+        fetchMedicationsForToday();
+        fetchLoggedMedications();
+    }, []);
 
     const handleMedicationAction = async (medicationId, action, time) => {
         try {
@@ -52,6 +65,14 @@ function MedicationHistory() {
         }
     };
 
+    useEffect(() => {
+        if (medications.length === 0) {
+            setAllTaken(true);
+        }
+    }, [medications]);
+
+    const messageAllTaken = `${user?.firstName || 'User'}, you've taken all your medications for today!`;
+
     const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
 
@@ -67,15 +88,18 @@ function MedicationHistory() {
                 ))}
             </div>
             <h2>Today’s Log</h2>
-            {medications.map((medication) => (
-                medication.reminder_times.map((time, index) => (
-                    <div key={`${medication.id}-${index}`} className="medication-reminder">
-                        <p>{user?.firstName || 'User'}, remember to take {medication.drug_name} at {time}</p>
-                        <button onClick={() => handleMedicationAction(medication.id, 'skipped', time)}>Skipped</button>
-                        <button onClick={() => handleMedicationAction(medication.id, 'taken', time)}>Taken</button>
-                    </div>
-                ))
-            ))}
+            {allTaken ? (
+                <p>{messageAllTaken}</p>
+            ) : (
+                medications.map((medication) => (
+                    medication.reminder_times.map((time, index) => (
+                        <div key={`${medication.id}-${index}`} className="medication-reminder">
+                            <p>{user?.firstName || 'User'}, remember to take {medication.drug_name} at {time}</p>
+                            <button onClick={() => handleMedicationAction(medication.id, 'skipped', time)}>Skipped</button>
+                            <button onClick={() => handleMedicationAction(medication.id, 'taken', time)}>Taken</button>
+                        </div>
+                    ))
+                )))}
 
             <h3>Logged today:</h3>
             <ul>
