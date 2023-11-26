927f62cef13d0d943ce433451b02aca83683b360
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-26 16:36:33 -0500

    refactored code for readability

--- src/components/MedicationHistory/MedicationHistory.js
+++ src/components/MedicationHistory/MedicationHistory.js
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import { AuthContext } from '../../auth/AuthContext';
 import './MedicationHistory.scss';
 
 function MedicationHistory() {
+    const { user } = useContext(AuthContext);
     const [medications, setMedications] = useState([]);
     const [loggedMedications, setLoggedMedications] = useState([]);
 
@@ -17,12 +19,15 @@ function MedicationHistory() {
             const response = await axios.get(`${SERVER_URL}/api/medications/today`, {
                 headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` }
             });
-            setMedications(response.data);
+            const medicationsWithParsedTimes = response.data.map(med => ({
+                ...med,
+                reminder_times: JSON.parse(med.reminder_times)
+            }));
+            setMedications(medicationsWithParsedTimes);
         } catch (error) {
             console.error('Error fetching medications for today:', error);
         }
     };
-
 
     const handleMedicationAction = async (medicationId, action) => {
         try {
@@ -55,12 +60,15 @@ function MedicationHistory() {
             </div>
             <h2>Today’s Log</h2>
             {medications.map((medication) => (
-                <div key={medication.id} className="medication-reminder">
-                    <p>{medication.name}, REMEMBER TO TAKE {medication.info} AT {medication.time}</p>
-                    <button onClick={() => handleMedicationAction(medication.id, 'skipped')}>Skipped</button>
-                    <button onClick={() => handleMedicationAction(medication.id, 'taken')}>Taken</button>
-                </div>
+                medication.reminder_times.map((time, index) => (
+                    <div key={`${medication.id}-${index}`} className="medication-reminder">
+                        <p>{user?.firstName || 'User'}, remember to take {medication.drug_name} at {time}</p>
+                        <button onClick={() => handleMedicationAction(medication.id, 'skipped')}>Skipped</button>
+                        <button onClick={() => handleMedicationAction(medication.id, 'taken')}>Taken</button>
+                    </div>
+                ))
             ))}
+
             <h3>Logged today:</h3>
             <ul>
                 {loggedMedications.map((medication) => (
